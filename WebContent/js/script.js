f89a68f3f251f9d8bea42b9e31ba5f3b4b5516f6
Emma Shakespeare <emma@wikblad.com>
2015-10-14 00:11:21 +0200

    setup

---
+++ WebContent/js/script.js
@@ -0,0 +1,21 @@
+function print(text){
+	
+	$("#out").append(text);
+}
+
+
+$(function(){
+	
+	$("#getdata").click(function(){
+		
+		$.ajax({
+			type:"GET",
+			url: "http://localhost:8080/moviez/api/collection/Scream",
+			datatype: "text", 
+			success: function(text){
+				print(text);
+			}
+		});
+	});
+	
+});
