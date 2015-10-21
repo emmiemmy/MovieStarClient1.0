d0a02114bba4786ee790b4915e4a349ff475cbf7
JodyONeill <jody.o.neill@gmail.com>
2015-10-21 14:10:54 +0200

    added titel, and actors

---
+++ bin/WebContent/js/script.js
@@ -0,0 +1,35 @@
+function outputToDocument(jsondata) {
+	$("#titelName").empty();
+	$("#actors").empty();
+	$("#plotText").empty();
+	$("#titelName").append(jsondata["Title"]);
+	$("#actors").append(jsondata["Actors"]);
+	$("#plotText").append(jsondata["Plot"]);
+	
+}
+
+function myFunction() {
+	var x = document.getElementById("myText").value;
+
+	document.getElementById("demo").innerHTML = x;
+}
+
+/**
+ * Function takes the user input and fetches the resource for chosen movie from omdb api
+ */
+$(function() {
+
+	$("#getMovieData").click(function() {
+				
+		var url = "http://localhost:8080/moviez/api/collection/" + $('#myText').val();
+
+		$.ajax({
+			type : "GET",
+			url : url,
+			datatype : "json",
+			success : function(jsondata) {
+				outputToDocument(jsondata);
+			}
+		});
+	});
+});
