88356e5c0296f5c1754776708909b7a963bc5fe5
Emma Shakespeare <emma@wikblad.com>
2015-10-21 13:50:53 +0200

    added html tags to display info about movie

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,6 +1,6 @@
 function outputToDocument(jsondata) {
 	$("#plotText").empty();
-	$("#plotText").append(jsondata["Plot"]);
+	$("#plotText").append("Titel:" + jsondata["Title"] + "\n" + "Handling:" +  jsondata["Plot"]);
 }
 
 function myFunction() {
