9b0eeb339ea22df5615a6dede9fb4e225305830d
Evelyn Gustavsson <Evy@Evelyns-MacBook-Air.local>
2016-01-07 16:50:39 +0100

    Har lagt till en API-knapp och lämnat in kompletteringen på It'sLearning.

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -4,10 +4,10 @@ function outputToDocument(jsondata) {
 	$("#plotText").empty();
 	$("#titelName").append("Titel: ");
 	$("#actors").append("Medverkande: ");
-	$("#plotText").append("Handling: ");	
+	$("#plotText").append("Handling: ");
 	$("#titelName").append(jsondata["Title"]);
 	$("#actors").append(jsondata["Actors"]);
-	$("#plotText").append(jsondata["Plot"]);	
+	$("#plotText").append(jsondata["Plot"]);
 }
 
 function outputTrailer(jsondata) {
@@ -22,57 +22,76 @@ function myFunction() {
 }
 
 $(document).keypress(function(e) {
-    if(e.which == 13) {
-        getMovieInfo();
-    }
+	if (e.which == 13) {
+		getMovieInfo();
+	}
 });
 
+/**
+ * Function that shows information about the API.
+ */
+$(function getAPI() {
+
+	$("#getAPI").click(
+			function() {
+
+				alert("1. \t/api/collection/{movietitle} "
+						+ "\n- Enter a movie title to get info about a movie \n"
+						+ "\n2. \t/api/version/check "
+						+ "\n- To check API version \n"
+						+ "\n3. \t/api/watchtrailer/{movietitle} "
+						+ "\n- Fetch trailer for movie");
+			});
+});
 
 /**
- * Function takes the user input and fetches the resource for chosen movie from omdb api
+ * Function takes the user input and fetches the resource for chosen movie from
+ * omdb api
  */
 $(function() {
-	
-	$("#getMovieData").click(function() {
-	
-				
-		var url = "http://localhost:8080/moviez/api/collection/" + $('#myText').val();
-		var urlTrailer = "http://localhost:8080/moviez/api/watchtrailer/" + $('#myText').val();
 
-		$.ajax({
-			type : "GET",
-			url : url,
-			datatype : "json",
-			success : function(jsondata) {
-				outputToDocument(jsondata);
-			}
-		});
-		
-		$.ajax({
-			type : "GET",
-			url : urlTrailer,
-			datatype : "json",
-			success : function(jsondata) {
-				if(jsondata  == null){
-//					$("#trailer").attr("src", "");
-					$("#trailer").attr("src", "http://i.imgur.com/W7mqS78.gif");
+	$("#getMovieData")
+			.click(
+					function() {
 
+						var url = "http://localhost:8080/moviez/api/collection/"
+								+ $('#myText').val();
+						var urlTrailer = "http://localhost:8080/moviez/api/watchtrailer/"
+								+ $('#myText').val();
 
+						$.ajax({
+							type : "GET",
+							url : url,
+							datatype : "json",
+							success : function(jsondata) {
+								outputToDocument(jsondata);
+							}
+						});
 
-				}else{
-					outputTrailer(jsondata);
-				}
-			}
-		});
-		
-	});
+						$.ajax({
+							type : "GET",
+							url : urlTrailer,
+							datatype : "json",
+							success : function(jsondata) {
+								if (jsondata == null) {
+									$("#trailer").attr("src",
+											"http://i.imgur.com/W7mqS78.gif");
+
+								} else {
+									outputTrailer(jsondata);
+								}
+							}
+						});
+
+					});
 });
 
 function getMovieInfo() {
-	
-	
-	var url = "http://localhost:8080/moviez/api/collection/" + $('#myText').val();
-	var urlTrailer = "http://localhost:8080/moviez/api/watchtrailer/" + $('#myText').val();
+
+	var url = "http://localhost:8080/moviez/api/collection/"
+			+ $('#myText').val();
+	var urlTrailer = "http://localhost:8080/moviez/api/watchtrailer/"
+			+ $('#myText').val();
 
 	$.ajax({
 		type : "GET",
@@ -82,26 +101,18 @@ function getMovieInfo() {
 			outputToDocument(jsondata);
 		}
 	});
-	
+
 	$.ajax({
 		type : "GET",
 		url : urlTrailer,
 		datatype : "json",
 		success : function(jsondata) {
-			if(jsondata  == null){
-//				$("#trailer").attr("src", "");
-//				$("#trailer").attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/PM5544_with_non-PAL_signals.png/250px-PM5544_with_non-PAL_signals.png");
-//				$("#trailer").attr("src", "http://www.platosclosetcolorado.com/images/not-found.gif");
+			if (jsondata == null) {
 				$("#trailer").attr("src", "http://i.imgur.com/W7mqS78.gif");
 
-
-
-
-			}else{
+			} else {
 				outputTrailer(jsondata);
 			}
 		}
 	});
-	
 };
-
